eb48d33a217700df815251b4107ca8dc2f1edc57
Iisakki Kulhua <iisakki@kulhua.net>
2024-09-22 15:08:54 +0300

    canary test

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.0.0')
+  res.send('1.11.12canaryOk')
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  //if (true) throw 'error...  '
+  res.send('OK')
 })
 
 app.listen(PORT, () => {
