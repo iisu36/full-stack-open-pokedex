be91ec1d6ddfad59033753016ca98f0ee96f6df1
Iisakki Kulhua <iisakki@kulhua.net>
2024-09-22 15:13:37 +0300

    canary error test and deleted extra fly.yml file

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.11.12canaryOk')
+  res.send('1.11.12canaryNotOk')
 })
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  //if (true) throw 'error...  '
+  if (true) throw 'error...  '
   res.send('OK')
 })
 
