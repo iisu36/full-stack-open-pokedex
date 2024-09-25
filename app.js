3b8d50ddff5350922546634a78eeed945863f344
Iisakki Kulhua <iisakki@kulhua.net>
2024-09-25 20:11:47 +0300

    11.19 testing with error values

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1.0.0')
+})
+
 app.get('/health', (req, res) => {
-  res.status(404).send('Not Found')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
+  res.send('OK')
 })
 
 app.listen(PORT, () => {
